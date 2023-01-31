66fd2f349952111d23e79274fb418ef45a1ec27b
Changmok Lee <mokcandy97@gmail.com>
2023-01-31 04:45:48 -0700

    fix error

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const toogleButton = document.querySelector('.navbar__toogleButton');
 const menu = document.querySelector('.navbar__menu');
-const icons = document.querySelector('.navber_icons');
+const icons = document.querySelector('.navbar__icons');
 
 toogleButton.addEventListener('click', () => {
     menu.classList.toggle('active');
