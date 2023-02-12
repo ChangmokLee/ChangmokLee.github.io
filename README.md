a398d07edf493749eb2a0fdb110a36d8b322bb0d
Changmok Lee <mokcandy97@gmail.com>
2023-02-11 23:51:25 -0700

    position practice

--- README.md
+++
@@ -1,1 +0,0 @@
-# ChangmokLee.github.io
--- main.js
+++
@@ -1,8 +0,0 @@
-const toogleButton = document.querySelector('.navbar__toogleButton');
-const menu = document.querySelector('.navbar__menu');
-const icons = document.querySelector('.navbar__icons');
-
-toogleButton.addEventListener('click', () => {
-    menu.classList.toggle('active');
-    icons.classList.toggle('active');
-})
